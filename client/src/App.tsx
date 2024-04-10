1285757716af0706551d61760cac4bc9f6474ec5
Reginald Mabanta <movah4ch@yahoo.com.ph>
2024-04-10 12:34:02 +0800

    added dashboard component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,13 @@ import { FC, ReactElement } from 'react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
 import { theme } from './theme/default.theme';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 const App: FC = (): ReactElement => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline>
-        <h1>Hello world</h1>
+        <Dashboard />
       </CssBaseline>
     </ThemeProvider>
   );
