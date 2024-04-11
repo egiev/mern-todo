b591e35ea6b1f6cfb58d51beada357937ba41740
Reginald Mabanta <movah4ch@yahoo.com.ph>
2024-04-11 14:49:02 +0800

    added task area component

--- client/src/components/content/content.tsx
+++ client/src/components/content/content.tsx
@@ -1,10 +1,36 @@
 import { FC, ReactElement } from 'react';
-import { Grid } from '@mui/material';
+import { Grid, Box } from '@mui/material';
+import dayjs from 'dayjs';
 
 export const Content: FC = (): ReactElement => {
   return (
     <Grid item md={8} px={4}>
-      <h1>Content Here</h1>
+      <Box mb={8} px={4}>
+        <h1>Status Of Your Task As On {dayjs().format('dddd, LL')}</h1>
+      </Box>
+
+      <Grid container display="flex" justifyContent="center">
+        <Grid
+          item
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-around"
+          alignItems="center"
+          md={10}
+          xs={12}
+          mb={8}
+        >
+          <Box>Counter</Box>
+          <Box>Counter</Box>
+          <Box>Counter</Box>
+        </Grid>
+
+        <Grid item display="flex" flexDirection="column" md={8} xs={10}>
+          <Box>Counter</Box>
+          <Box>Counter</Box>
+          <Box>Counter</Box>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
