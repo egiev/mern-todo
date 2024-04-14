76eda767e5d0b4380c953bfdb0b71cedfcf0a647
Reginald Mabanta <movah4ch@yahoo.com.ph>
2024-04-14 14:58:19 +0800

    added cors and body-parser library

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,15 @@
 import express, { Express, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 import './config/dotenv';
 import { AppDataSource } from './config/database';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (_, response: Response) => {
   response.send('Hello world');
