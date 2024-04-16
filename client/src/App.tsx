8d23d0773db5d8c50536295f7e439fe4b434410f
Reginald Mabanta <movah4ch@yahoo.com.ph>
2024-04-16 16:43:22 +0800

    added react-query and react-query-devtools

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,24 @@
 import { FC, ReactElement } from 'react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { theme } from './theme/default.theme';
 import { Dashboard } from './pages/dashboard/dashboard';
 
+const queryClient: QueryClient = new QueryClient();
+
 const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline>
-        <Dashboard />
-      </CssBaseline>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <Dashboard />
+        </CssBaseline>
+      </ThemeProvider>
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
